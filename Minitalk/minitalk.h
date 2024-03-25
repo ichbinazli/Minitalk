913c9590707816f9333eb66c79d6ea3aa8561a71
Nazlı Anapa <114147817+ichbinazli@users.noreply.github.com>
2024-03-25 16:29:09 +0300

    Add files via upload

---
+++ Minitalk/minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: naanapa <naanapa@student.42kocaeli.com.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/25 12:53:41 by naanapa           #+#    #+#             */
+/*   Updated: 2024/03/25 12:53:44 by naanapa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <unistd.h>
+
+int		atoi(const char *str);
+void	putnbr(int nbr);
+
+#endif
